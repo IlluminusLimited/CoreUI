5f4d92ab24d8eab137455e00fff4f572c66d52d4
scytherswings <scytherswings@gmail.com>
2019-02-01 15:03:58 -0500

    pagination for images

--- components/Collectables/Collectable.js
+++ components/Collectables/Collectable.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Appbar, Text, Card, Title, Paragraph} from 'react-native-paper';
-import Carousel from "react-native-snap-carousel";
+import Carousel, {Pagination} from "react-native-snap-carousel";
 import PropTypes from 'prop-types'
 import Layout from "../../constants/Layout";
 
@@ -21,7 +21,8 @@ class Collectable extends Component {
     this.state = {
       collectableId: (collectableId ? collectableId : this.props.collectableId),
       collectable: {},
-      loaded: false
+      loaded: false,
+      activeSlide: 0
     };
   }
 
@@ -57,6 +58,29 @@ class Collectable extends Component {
     );
   }
 
+  get pagination() {
+    const {collectable, activeSlide} = this.state;
+    return (
+      <Pagination
+        dotsLength={collectable.images.length}
+        activeDotIndex={activeSlide}
+        containerStyle={styles.carouselPagination}
+        dotStyle={{
+          width: 10,
+          height: 10,
+          borderRadius: 5,
+          marginHorizontal: 8,
+          backgroundColor: 'rgba(255, 255, 255, 0.92)'
+        }}
+        inactiveDotStyle={{
+          // Define styles for inactive dots here
+        }}
+        inactiveDotOpacity={0.4}
+        inactiveDotScale={0.6}
+      />
+    );
+  }
+
   // Carousel sliderWidth and itemWidth are important, if you change the stylesheet make sure this
   // still a valid setup.
   render() {
@@ -73,9 +97,11 @@ class Collectable extends Component {
                     }}
                     data={this.state.collectable.images}
                     renderItem={this._renderItem}
+                    onSnapToItem={(index) => this.setState({activeSlide: index})}
                     sliderWidth={Layout.window.width}
                     itemWidth={Layout.window.width - 10}
                   />
+                  {this.pagination}
                 </View>
                 <View style={styles.collectableDetails}>
                   <Text>Name: {this.state.collectable.name}</Text>
@@ -108,13 +134,15 @@ const styles = StyleSheet.create({
   },
   container: {
     flex: 1,
+    backgroundColor: 'rgba(0, 0, 0, 0.75)',
   },
   carouselContainer: {
-    flex: 1,
+    flex: 2,
     margin: 5,
     alignItems: 'center',
     justifyContent: 'center',
-
+  },
+  carouselPagination: {
   },
   collectableDetails: {
     flex: 1,
