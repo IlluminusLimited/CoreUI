2662549fb33baad85edc1e0270e6b18631890e97
scytherswings <scytherswings@gmail.com>
2019-02-01 15:26:02 -0500

    we are going to need to write our own sdk client to handle 404s and other such things

--- screens/Collections.js
+++ screens/Collections.js
@@ -1,18 +1,59 @@
 import React from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
+import {ScrollView, StyleSheet, View} from 'react-native';
 import {Text} from "react-native-paper";
 import {Ionicons} from "react-native-vector-icons";
+import {Collectables} from "../components/Collectables";
 
 export default class Collections extends React.Component {
-  // static navigationOptions = {
-  //   tabBarIcon: <Ionicons name={'md-home'} size={24}/>
-  // };
+  static navigationOptions = ({navigation, navigationOptions}) => {
+    return {
+      header: null
+    };
+  };
+
+  state = {
+    loaded: false,
+    collections: []
+  };
+
+  componentDidMount() {
+    this._fetchPins();
+  }
+
+  _fetchPins() {
+    fetch('https://api-dev.pinster.io/v1/collections?page%5Bsize%5D=15')
+      .then(results => {
+        console.log(results);
+        return results.json()
+      })
+      .then(collections => {
+        console.log(collections);
+        this.setState({
+          collections: collections.data,
+          loaded: true
+        })
+      })
+      .catch(error => console.error('error getting collections', error));
+  }
 
   render() {
     return (
-      <ScrollView style={styles.container}>
-        <Text>Collections go here</Text>
-      </ScrollView>
+      <View style={styles.container}>
+        <ScrollView
+          style={styles.container}
+          contentContainerStyle={styles.contentContainer}
+        >
+          {this.state.loaded ? (
+            this.state.collectables.length !== 0 ? (
+              <Collectables collectableData={this.state.collectables} navigation={this.props.navigation} />
+            ) : (
+              <Text>Your search query returned no results. Try something else.</Text>
+            )
+          ) : (
+            <Text>LOADING ZOMG</Text>
+          )}
+        </ScrollView>
+      </View>
     );
   }
 }
@@ -20,9 +61,9 @@ export default class Collections extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    height: 100 + '%',
-    width: 100 + '%',
-    backgroundColor: '#fff',
-    marginTop: 25
-  }
+    backgroundColor: '#fff'
+  },
+  contentContainer: {
+    paddingTop: 10
+  },
 });
--- screens/Home.js
+++ screens/Home.js
@@ -18,6 +18,19 @@ export default class Home extends Component {
     this._fetchPins();
   }
 
+  _fetchPins() {
+    fetch('https://api-dev.pinster.io/v1/pins?page%5Bsize%5D=15')
+      .then(results => results.json())
+      .then(collectables => {
+        console.log(collectables);
+        this.setState({
+          collectables: collectables.data,
+          loaded: true
+        })
+      })
+      .catch(error => console.error('error getting all pins', error));
+  }
+
   render() {
     return (
       <View style={styles.container}>
@@ -25,8 +38,6 @@ export default class Home extends Component {
           style={styles.container}
           contentContainerStyle={styles.contentContainer}
         >
-          {console.log("NAVIGATION:", this.props.navigation)}
-
           {this.state.loaded ? (
             this.state.collectables.length !== 0 ? (
               <Collectables collectableData={this.state.collectables} navigation={this.props.navigation} />
@@ -41,18 +52,7 @@ export default class Home extends Component {
     );
   }
 
-  _fetchPins() {
-    fetch('https://api-dev.pinster.io/v1/pins?page%5Bsize%5D=15')
-      .then(results => results.json())
-      .then(collectables => {
-        console.log(collectables);
-        this.setState({
-          collectables: collectables.data,
-          loaded: true
-        })
-      })
-      .catch(error => console.error('error getting all pins', error));
-  }
+
 }
 
 const styles = StyleSheet.create({
