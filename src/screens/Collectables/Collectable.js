c100c17d638393d111c53d467451b52d89d8ab96
scytherswings <scytherswings@gmail.com>
2019-04-01 20:53:01 -0400

    readme

--- src/screens/Collectables/Collectable.js
+++ src/screens/Collectables/Collectable.js
@@ -50,10 +50,6 @@ class Collectable extends Component {
     return (
       <Card style={styles.card}>
         <Card.Cover source={{uri: item.storage_location_uri + '_1000x1000'}} />
-        <Card.Content style={styles.cardContent}>
-          <Title>asdfasgegegedf{item.name}</Title>
-          <Paragraph>sadfadsfadsf{item.description}</Paragraph>
-        </Card.Content>
       </Card>
     );
   }
