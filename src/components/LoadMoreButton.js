33d9a198bf14d2433d2bb217e6c4ff7baa8b2848
scytherswings <scytherswings@gmail.com>
2019-04-07 16:22:45 -0400

    load more is better now

--- src/components/LoadMoreButton.js
+++ src/components/LoadMoreButton.js
@@ -6,14 +6,16 @@ import PropTypes from "prop-types";
 export default class LoadMoreButton extends React.Component {
   render() {
     return (
-      <View style={this.props.style}>
+      <View>
         {(this.props.nextPage === '' || this.props.nextPage === null || this.props.nextPage === undefined) ?
           (
             null
           ) : (
-            <Button style={{mode: 'contained', dark: true}} onPress={this.props.fetchMoreItems}>
-              Load more
-            </Button>
+            <View style={this.props.style}>
+              <Button mode={'contained'} dark={true} onPress={this.props.fetchMoreItems}>
+                Load more
+              </Button>
+            </View>
           )}
       </View>
     )
