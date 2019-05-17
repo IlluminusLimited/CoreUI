bbc124519c6c33b97c89c5d653dd254eefb7548d
scytherswings <scytherswings@gmail.com>
2019-05-17 13:23:55 -0400

    some work on our story, not completey fleshed out yet

--- src/screens/OurStory.js
+++ src/screens/OurStory.js
@@ -1,14 +1,13 @@
 import React from 'react';
-import {View, ScrollView, StyleSheet} from "react-native";
+import {ScrollView, StyleSheet, View} from "react-native";
+import {Avatar, Card, Divider, Headline, Paragraph, Subheading, Text, Title} from "react-native-paper";
 import {withNavigation} from "react-navigation";
-
-import {Card, Paragraph, Subheading, Title} from "react-native-paper";
 
 
 class OurStory extends React.Component {
   static navigationOptions = ({navigation, navigationOptions}) => {
     return {
-      header: null,
+      title: "Our Story",
     };
   };
 
@@ -24,23 +23,6 @@ class OurStory extends React.Component {
     return (
       <ScrollView style={styles.container}>
         <View style={styles.infoContainer}>
-          <Title>Our Story</Title>
-
-          <Card style={styles.card}>
-            <Card.Title title="Our Core Values" />
-            <Card.Content>
-              <Card.Cover source={require('../../assets/images/our_core_values.png')} />
-
-              <Subheading>We stand behind these core values</Subheading>
-              <Paragraph>
-                Community First
-              </Paragraph>
-                <Paragraph>The DI pin trading community is at the heart and soul of our project.
-                  All its members are valuable to us and deserve the best pin trading experience possible.
-                </Paragraph>
-              <Paragraph>It is our greatest pleasure to give back to the DI community.</Paragraph>
-            </Card.Content>
-          </Card>
 
           <Card style={styles.card}>
             <Card.Title title="Who we are" />
@@ -51,6 +33,31 @@ class OurStory extends React.Component {
                 this project in order to create a better pin trading experience for everyone.
               </Paragraph>
               <Paragraph>It is our greatest pleasure to give back to the DI community.</Paragraph>
+            </Card.Content>
+          </Card>
+
+          <Card style={styles.card}>
+            <Card.Title title="Our Core Values" />
+            <Card.Content>
+              <Card.Cover source={require('../../assets/images/our_core_values.png')} />
+              <Subheading>Community First</Subheading>
+              <Paragraph>The DI pin trading community is at the heart and soul of our project.
+                All its members are valuable to us and deserve the best pin trading experience possible.
+              </Paragraph>
+              <Paragraph>We believe in fostering ties of strength and friendship between DIers across the world through pin trading.</Paragraph>
+            </Card.Content>
+          </Card>
+          <Card style={styles.card}>
+            <Card.Title title="Code of Conduct" />
+            <Card.Content>
+              <Card.Cover source={require('../../assets/images/pinster_header.png')} />
+              <Subheading>Help us make pin trading a great experience for all!</Subheading>
+              <Paragraph>{'\u2022 '}I will trade pins fairly with all other traders I encounter and will not take
+                advantage of other pin traders in any way.</Paragraph>
+              <Paragraph>{'\u2022 '}I will treat others with patience, kindness, and courtesy.</Paragraph>
+              <Paragraph>{'\u2022 '}I will respect another pin trader’s right to decline a trade.</Paragraph>
+              <Paragraph>{'\u2022 '}I will pick up after myself and throw away all pin trading trash
+                I create.</Paragraph>
             </Card.Content>
           </Card>
         </View>
@@ -66,7 +73,7 @@ const styles = StyleSheet.create({
     infoContainer: {
       flex: 1,
       margin: 20,
-      paddingTop: 20
+      // paddingTop: 20
     },
     userInfo: {
       marginTop: 35,
@@ -81,6 +88,11 @@ const styles = StyleSheet.create({
       flex: 1,
       margin: 10,
       elevation: 4,
+    },
+    bulletPoint: {
+      textAlignVertical: 'bottom',
+      fontSize: 16,
+      includeFontPadding: false,
     },
   }
 );
